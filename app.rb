ab368636ddc8e026ee5d6ac9e86b243018b8ca9d
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-03 01:36:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,6 +21,6 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do 
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
-    "#{ @num1 * @num2}"
+    "#{ @num1 } multiplied by #{ @num2 } equals #{ @num1 * @num2}"
   end 
 end
